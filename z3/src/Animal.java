23caaf2c9b0ca9dfd517218aff828c430c2afe48
temachtech <133698888+tmbhh@users.noreply.github.com>
2024-09-24 10:06:01 +0300

    Update Animal.java

--- z3/src/Animal.java
+++ z3/src/Animal.java
@@ -4,37 +4,71 @@ public class Animal {
     }
 }
 
-
- class Dog extends  Animal{
-     @Override
-     public String say() {
-         return super.say('Dog goes woof');
-     }
- }
-class Cat extends  Animal{
-
+class Dog extends Animal {
+    @Override
+    public String say() {
+        return "woof";
+    }
 }
-class Bird extends  Animal{
-
+class Cat extends Animal {
+    @Override
+    public String say() {
+        return "meow";
+    }
 }
-class Mouse extends  Animal{
 
+class Bird extends Animal {
+    @Override
+    public String say() {
+        return "tweet";
+    }
 }
-class Cow extends  Animal{
 
+class Mouse extends Animal {
+    @Override
+    public String say() {
+        return "squeek";
+    }
 }
-class Frog extends  Animal{
 
+class Cow extends Animal {
+    @Override
+    public String say() {
+        return "moo";
+    }
 }
-class Elephant extends  Animal{
 
+class Frog extends Animal {
+    @Override
+    public String say() {
+        return "croak";
+    }
 }
-class Duck extends  Animal{
 
+class Elephant extends Animal {
+    @Override
+    public String say() {
+        return "toot";
+    }
 }
-class Fish extends  Animal{
 
+class Duck extends Animal {
+    @Override
+    public String say() {
+        return "quack";
+    }
 }
-class Seal extends  Animal{
 
+class Fish extends Animal {
+    @Override
+    public String say() {
+        return "blub";
+    }
+}
+
+class Seal extends Animal {
+    @Override
+    public String say() {
+        return "ow ow ow";
+    }
 }
